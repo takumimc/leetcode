02a4594b3f72568fd026c747b67b5bed173d961f
Ian McIntyre <ianmcintyre12@hotmail.com>
2023-08-20 16:31:20 -0700

    121

---
+++ 121.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        max_profit = 0
+        for i in range(0,len(prices) + 1):
+            for j in range(i+1,len(prices)):
+                profit = prices[j] - prices[i]
+                if profit > max_profit:
+                    max_profit = profit
+
+        return max_profit
