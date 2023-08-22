4f121ba3a513556eb4e518478379fa5ad2ca7dec
Ian McIntyre <ianmcintyre12@hotmail.com>
2023-08-21 17:39:23 -0700

    20.py

---
+++ 20.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        starters =  []
+        left = ['(','[','{']
+        right = {
+            ')':'(',
+            ']':'[',
+            '}':'{',
+        }
+
+        for char in s:
+            if char not in left and len(starters) == 0:
+                return False
+            if char in left:
+                starters.insert(0, char)
+            else:
+                if right[char] == starters[0]:
+                    starters.pop(0)
+                else:
+                    return False
+        if len(starters) == 0:
+            return True
+        else:
+            return False
